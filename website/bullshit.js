dbe45ab7e420d7d03e0f71a07a523d16cc9c568b
Luka <lukaadomu678@gmail.com>
2026-09-19 11:52:47 +0300

    Fixed Js code

--- website/bullshit.js
+++ website/bullshit.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var allimg = document.getElementsByTagName('titleimg');
 var imgs = ["img/title/title_0.png","img/title/title_1.png","img/title/title_2.png","img/title/title_3.png","img/title/title_4.png"];
 
@@ -22,14 +21,3 @@ async function gameLoop() {
     isActive = true;
     gameLoop();
 };
-=======
-var imgs = ["img/title/title_0.png","img/title/title_1.png","img/title/title_2.png","img/title/title_3.png","img/title/title_4.png"]
-
-setInterval(() => {
-  imgs.forEach(function (l, index) {
-  setTimeout(function () {
-    document.getElementById("imgseq").style.backgroundImage = "url(" + l + ")";
-  }, index * 500);
-});
-},500);
->>>>>>> e7a4b653944cad82adf0729553acab97cc12f6cb
